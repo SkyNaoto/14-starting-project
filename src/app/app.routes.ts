abded44de50327fbd531ee33896a6c5840c5adf9
SkyNaoto <naoto_70102@yahoo.co.jp>
2025-07-20 10:02:45 +0900

    Finished 281

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from "@angular/router";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
-
-import { TasksComponent } from "./tasks/tasks.component";
-import { NewTaskComponent } from "./tasks/new-task/new-task.component";
+import { routes as userRoutes} from "./users/users.routes";
 import { NotFoundComponent } from "./not-found/not-found.component";
 
 export const routes: Routes = [
@@ -16,22 +14,7 @@ export const routes: Routes = [
     {
         path: 'users/:userId', // <your-domain>/users/<uid>
         component: UserTasksComponent,
-        children: [
-            {
-                path: '',
-                redirectTo: 'tasks',
-                pathMatch: 'full'
-
-            },
-            {
-                path: 'tasks', // <your-domain>/users/<uid>/tasks
-                component: TasksComponent
-            },
-            {
-                path: 'tasks/new',
-                component: NewTaskComponent
-            }
-        ]
+        children: userRoutes,
     },
     {
         path: '**',
